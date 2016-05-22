d2292af00367f9e5748eea9e62054ea0dc2bdfa0
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-05-22 22:04:02 +0200

    2016-05-22 first commit, intro to py, integers

---
+++ ex01.py
@@ -0,0 +1,24 @@
+a = 10
+b = 3.14
+c = True
+d = "hello"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(10 // 3)
+print(10 / 3)
+print(10 ** 3)
+print(-59)
+print(a - 4)
+a += 21
+print(a)
+a /= 3
+print(a)
+print(15 % 4)
+# divmod() zwraca tuple
+print(divmod(15, 4))
+print(divmod(9, 5))
+# s. 24 - Bases
